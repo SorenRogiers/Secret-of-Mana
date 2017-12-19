d66b7e683fd2e162d1f7d1f8bd2a14c07fc005aa
Sören Rogiers <soren.rogiers@student.howest.be>
2017-12-19 21:18:28 +0100

    Project setup - Added camera script - Added GameManager script that inherits from the Singleton script

---
+++ SecretOfMana/Assets/Scripts/Camera/PlayerCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    private Transform _cameraTarget;
+
+    private Vector3 _cameraOffset = new Vector3(0, 10, -5);
+    private float _speed = 5.0f;
+
+    public void SetCameraTarget(Transform target)
+    {
+        _cameraTarget = target;
+    }
+
+    private void Update()
+    {
+        if (_cameraTarget == null)
+        {
+            Debug.LogError("Camera target is not set!",_cameraTarget);
+            return;
+        }
+
+        transform.position = Vector3.Lerp(this.transform.position, _cameraTarget.position + _cameraOffset, Time.deltaTime *_speed);
+    }
+}
---
+++ SecretOfMana/Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    private static bool _IsApplicationQuitting = false;
+    private static object _lock = new object();
+
+    public static T Instance()
+    {
+        if (_IsApplicationQuitting)
+        {
+            Debug.LogWarning("Singleton Instance of " + typeof(T) + " is already destroyed by the application");
+            return null;
+        }
+
+        lock (_lock)
+        {
+            if (_instance == null)
+            {
+                //Check in scene if it exists
+                _instance = FindObjectOfType(typeof(T)) as T;
+
+                //If there are more, we got a problem
+                if (FindObjectsOfType(typeof(T)).Length > 1)
+                {
+                    Debug.LogError("More than one instance of singleton" + typeof(T));
+                    return _instance;
+                }
+
+                //Still no instance, I need to create one
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).ToString());
+                    _instance = obj.AddComponent<T>();
+                    DontDestroyOnLoad(obj);
+                }
+            }
+        }
+
+        return _instance;
+    }
+
+    protected Singleton()
+    {
+    }
+
+    public void OnDestroy()
+    {
+        _IsApplicationQuitting = true;
+    }
+}
